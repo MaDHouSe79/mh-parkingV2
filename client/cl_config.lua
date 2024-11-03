de823c3c76dcc015640aaf0586e9cc6f9e1d92f0
MaDHouSe <madhouse1979@gmail.com>
2024-11-03 21:03:04 +0100

    Update cl_config.lua

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -4,13 +4,13 @@
 Config = {} -- Placeholder don't edit or change or remove this.
 
 -- Target Script
-Config.TargetScript = "ox_target" -- Default qb-target but you can also use ox_target.
+Config.TargetScript = "qb-target" -- Default qb-target but you can also use ox_target.
 
 -- Fuel Script
-Config.FuelScript = 'mh-fuel'     -- Default is LegacyFuel, if you use a other fuel script, for example cc-fuel
+Config.FuelScript = 'LegacyFuel'  -- Default is LegacyFuel, if you use a other fuel script, for example cc-fuel
 
 -- Notify Script
-Config.DisableParkNotify = true   -- Default true, if false you get many notifications when you enter or leave the vehicle, all other notify massages are stil enable.
+Config.DisableParkNotify = false  -- Default true, if false you get many notifications when you enter or leave the vehicle, all other notify massages are stil enable.
 Config.NotifyScript = "k5_notify" -- Default qb, but you can use this scripts aswell (k5_notify, okokNotify, Roda_Notifications)
 
 Config.ForceVehicleOnGound = true -- If true this will force parked vehicles on the ground, sometimes the vehicles are in the air and by enable this is force the vehicle to the ground
