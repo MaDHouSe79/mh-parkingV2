e306191af42e832b42fb83901be904d4a1742a33
MaDHouSe <madhouse1979@gmail.com>
2024-11-04 20:03:16 +0100

    Update nl.lua

--- locales/nl.lua
+++ locales/nl.lua
@@ -2,10 +2,10 @@ local Translations = {
     info = {
         ['not_the_owner'] = "Je bent niet de eigenaar van dit voertuig...",
         ['remove_vehicle_zone'] = "Voertuig verwijderd uit de parkeerzone",
-        ['limit_parking'] = "U heeft de parkeerlimiet bereikt. Limiet((%limit))",
+        ['limit_parking'] = "U heeft de parkeerlimiet bereikt. Limiet(%{limit})",
         ['vehicle_parked'] = "Voertuig staat geparkeerd",
         ['already_parked'] = "Voertuig staat al geparkeerd",
-        ['parked_blip'] = "Geparkeerd: {%model}",
+        ['parked_blip'] = "Geparkeerd: %{model}",
         ['no_vehicle_nearby'] = "Geen voertuig in de buurt",
     },
     target = {
