a789480d97b0604fb5c19c9aa3c519dec22ea5ce
MaDHouSe <madhouse1979@gmail.com>
2024-11-11 16:56:54 +0100

    Delete server/sv_config.lua

--- server/sv_config.lua
+++
@@ -1,8 +0,0 @@
-SV_Config = {}               -- Placeholder don't edit or change this.
-
-SV_Config.Maxparking = 5     -- Default 5, this is max parked vehicles allowed per player, don't go to high with this...
-
-SV_Config.AutoSave = true    -- Default true, auto save player owned vehicle locations, this is dependend on the `SV_Config.AutoSaveTimer`
-SV_Config.AutoSaveTimer = 10 -- Default 10 secs, you can go higher if you want, don't go to low.
-
-SV_Config.Debug = false      -- Default false, if true this will show prints in the server console.
