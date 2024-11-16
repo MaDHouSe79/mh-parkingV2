3b177d282d12a6aa39fb18db64e42419e11eb1e7
MaDHouSe <madhouse1979@gmail.com>
2024-11-16 16:44:24 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 author 'MaDHouSe79'
-description 'MH ParkingV2 - A Realistic Vehicle Parking'
-version '1.0.0'
+description 'MH ParkingV2 - A Realistic Vehicle Parking System By MaDHouSe79'
+version '1.0.1'
 repository 'https://github.com/MaDHouSe79/mh-parkingV2'
 shared_scripts {'@ox_lib/init.lua', 'shared/locale.lua', 'locales/en.lua', 'shared/config.lua'}
 client_scripts {'client/main.lua'}
