203dc40dff23d2e43b06170d2b24e34d34b5bb25
MaDHouSe <madhouse1979@gmail.com>
2025-03-30 11:29:55 +0200

    Update server.lua

--- core/functions/server.lua
+++ core/functions/server.lua
@@ -227,22 +227,6 @@ function Parking.Functions.LeftVehicle(src, currentSeat, netId)
     end
 end
 
-local function GetSinglePlayerId()
-    if playerId == -1 then
-        local players = GetPlayers()
-        for k, v in pairs(players) do
-            if v.PlayerData ~= nil and v.PlayerData.source ~= nil and type(v.PlayerData.source) == 'number' and v.PlayerData.source > 0 then
-                playerId = v.PlayerData.source
-                break
-            end
-        end
-        return
-    elseif playerId ~= -1 and playerId > 0 then
-        playerId = -1
-        return
-    end
-end
-
 function Parking.Functions.RefreshVehicles(src, onStart)
     if onStart then
         if playerId == -1 then GetSinglePlayerId() end
