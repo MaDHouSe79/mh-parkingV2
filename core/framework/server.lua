363aceb44eb5a0a6080958e1dfc012197e549758
MaDHouSe <madhouse1979@gmail.com>
2025-03-31 12:29:19 +0200

    Update server.lua

--- core/framework/server.lua
+++ core/framework/server.lua
@@ -1,13 +1,10 @@
 --[[ ===================================================== ]] --
 --[[       MH Realistic Parking V2 Script by MaDHouSe      ]] --
 --[[ ===================================================== ]] --
-Framework = nil
-CreateCallback = nil
-AddCommand = nil
-playerId = -1
+Framework, CreateCallback, AddCommand, playerId = nil, nil, nil, -1
 
 if GetResourceState("es_extended") ~= 'missing' then
-    Config.Framework = 'esx'
+    SV_Config.Framework = 'esx'
     Framework = exports['es_extended']:getSharedObject()
     CreateCallback = Framework.RegisterServerCallback
     AddCommand = Framework.RegisterCommand
@@ -16,7 +13,7 @@ if GetResourceState("es_extended") ~= 'missing' then
     function GetJob(source) return Framework.GetPlayerFromId(source).job end
     function Notify(src, message, type, length) TriggerClientEvent("mh-parkingV2:client:Notify", src, message, type, length) end
 elseif GetResourceState("qb-core") ~= 'missing' then
-    Config.Framework = 'qb'
+    SV_Config.Framework = 'qb'
     Framework = exports['qb-core']:GetCoreObject()
     CreateCallback = Framework.Functions.CreateCallback
     AddCommand = Framework.Commands.Add
@@ -25,7 +22,7 @@ elseif GetResourceState("qb-core") ~= 'missing' then
     function GetJob(source) return Framework.Functions.GetPlayer(source).PlayerData.job end
     function Notify(src, message, type, length) TriggerClientEvent("mh-parkingV2:client:Notify", src, message, type, length) end
 elseif GetResourceState("qbx-core") ~= 'missing' then
-    Config.Framework = 'qbx'
+    SV_Config.Framework = 'qbx'
     Framework = exports['qbx-core']:GetCoreObject()
     CreateCallback = Framework.Functions.CreateCallback
     AddCommand = Framework.Commands.Add
@@ -52,22 +49,22 @@ end
 function CreateVehicleList()
     local result = nil
     local vehicles = {}
-    if Config.Framework == 'esx' then
+    if SV_Config.Framework == 'esx' then
         result = MySQL.Sync.fetchAll("SELECT * FROM owned_vehicles WHERE stored = ?", {3})
-    elseif Config.Framework == 'qb' or Config.Framework == 'qbx' then
+    elseif SV_Config.Framework == 'qb' or SV_Config.Framework == 'qbx' then
         result = MySQL.Sync.fetchAll("SELECT * FROM player_vehicles WHERE state = ?", {3})
     end
     if type(result) == 'table' then
         for k, v in pairs(result) do
             local fullname = "unknow"
-            if Config.Framework == 'esx' then
+            if SV_Config.Framework == 'esx' then
                 local char = MySQL.Sync.fetchAll("SELECT * FROM users WHERE owner = ?", {v.citizenid})[1]
                 if char then fullname = char.firstname.. ' ' ..char.lastname end
                 local tmpVehicles = MySQL.Sync.fetchAll("SELECT * FROM owned_vehicles WHERE stored = ? AND owner = ?", {3, v.citizenid})[1]
                 local mods = json.decode(tmpVehicles.vehicle)
                 local coords = json.decode(tmpVehicles.location)
                 vehicles[#vehicles + 1] = {citizenid = tmpVehicles.owner, fullname = fullname, plate = tmpVehicles.plate, model = mods.model, fuel = mods.fuelLevel, engine = mods.engineHealth, body = mods.bodyHealth, mods = mods, location = coords}
-             elseif Config.Framework == 'qb' or Config.Framework == 'qbx' then
+             elseif SV_Config.Framework == 'qb' or SV_Config.Framework == 'qbx' then
                 local target = Framework.Functions.GetPlayerByCitizenId(v.citizenid) or Framework.Functions.GetOfflinePlayerByCitizenId(v.citizenid)
                 fullname = target.PlayerData.charinfo.firstname .. ' ' .. target.PlayerData.charinfo.lastname
                 local tmpVehicles = MySQL.Sync.fetchAll("SELECT * FROM player_vehicles WHERE state = ? and plate = ?", {3, v.plate})[1]
