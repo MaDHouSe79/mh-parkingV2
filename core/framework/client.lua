30d8a116d7109c64976f4dbec2304e3db5741785
MaDHouSe <madhouse1979@gmail.com>
2025-03-31 11:34:12 +0200

    Update client.lua

--- core/framework/client.lua
+++ core/framework/client.lua
@@ -43,25 +43,6 @@ elseif GetResourceState("qbx-core") ~= 'missing' then
     RegisterNetEvent('QBCore:Player:SetPlayerData', function(data) PlayerData = data end)
 end
 
-function Trim(value)
-    if not value then return nil end
-    return (string.gsub(value, '^%s*(.-)%s*$', '%1'))
-end
-
-function Round(value, numDecimalPlaces)
-    if not numDecimalPlaces then return math.floor(value + 0.5) end
-    local power = 10 ^ numDecimalPlaces
-    return math.floor((value * power) + 0.5) / (power)
-end
-
-function SamePlates(plate1, plate2)
-    return (Trim(plate1) == Trim(plate2))
-end
-
-function GetDistance(pos1, pos2)
-    return #(vector3(pos1.x, pos1.y, pos1.z) - vector3(pos2.x, pos2.y, pos2.z))
-end
-
 function LoadModel(model)
     while not HasModelLoaded(model) do
         RequestModel(model)
