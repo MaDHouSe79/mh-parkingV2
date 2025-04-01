3864a28cf43b59a064da16c07a4bbf36e97af47a
MaDHouSe <madhouse1979@gmail.com>
2025-04-01 16:39:39 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 author 'MaDHouSe79'
 description 'MH ParkingV2 - A Realistic Vehicle Parking System'
-version '1.0.11'
+version '1.0.12'
 repository 'https://github.com/MaDHouSe79/mh-parkingV2'
 shared_scripts {
-    -- '@qbx_core/modules/lib.lua', -- only use this of you use qbx Framework
     '@ox_lib/init.lua',
     'shared/locale.lua',
     'locales/en.lua',
