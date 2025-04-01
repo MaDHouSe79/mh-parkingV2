0d32a2f0cd33b2e516764462b9b922781180b410
MaDHouSe <madhouse1979@gmail.com>
2025-04-01 19:26:02 +0200

    Update functions.lua

--- shared/functions.lua
+++ shared/functions.lua
@@ -34,33 +34,30 @@ end
 
 entityEnumerator = {
     __gc = function(enum)
-      if enum.destructor and enum.handle then
-        enum.destructor(enum.handle)
-      end
-      enum.destructor = nil
-      enum.handle = nil
+        if enum.destructor and enum.handle then
+            enum.destructor(enum.handle)
+        end
+        enum.destructor = nil
+        enum.handle = nil
     end
-  }
+}
 
 function EnumerateEntities(initFunc, moveFunc, disposeFunc)
     return coroutine.wrap(function()
-      local iter, id = initFunc()
-      if not id or id == 0 then
+    local iter, id = initFunc()
+    if not id or id == 0 then
         disposeFunc(iter)
         return
-      end
-
-      local enum = {handle = iter, destructor = disposeFunc}
-      setmetatable(enum, entityEnumerator)
-
-      local next = true
-      repeat
+    end
+    local enum = {handle = iter, destructor = disposeFunc}
+    setmetatable(enum, entityEnumerator)
+    local next = true
+    repeat
         coroutine.yield(id)
         next, id = moveFunc(iter)
-      until not next
-
-      enum.destructor, enum.handle = nil, nil
-      disposeFunc(iter)
+    until not next
+        enum.destructor, enum.handle = nil, nil
+        disposeFunc(iter)
     end)
 end
 
